48347313c31b8f19cd02b7b376b6d3279c0a309a
sanchirmaa <sanchirma8060@gmail.com>
2023-04-18 16:06:00 +0900

    Shoo haydag towchiig ajilladag bolgoson

--- app.js
+++ app.js
@@ -10,17 +10,20 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Shoonii ali talaaraa buusniig hadgalah huwisagch, 1-6 gesen toog ene huwisagchind sanamsarguigeer ugnu
- var dice = Math.floor(Math.random() * 6) +1;
-
-//  document.querySelector("#score-0").textContent = dice;
-//  document.querySelector("#score-1").innerHTML = "<em>Hey</em>";
 
 // Togloom ehlehed beldej onoog 0 bolgoh
-document.querySelector("#score-0").textContent = "0";
-document.querySelector("#score-1").textContent = "0";
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = "0";
-document.querySelector("#current-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".dice").style.display = "none";
- console.log("Dice:"  + dice);
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector('.btn-roll').addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random() * 6) +1;
+    diceDom.style.display = "block";
+    diceDom.src ="dice-" + diceNumber + ".png";
+});
+
